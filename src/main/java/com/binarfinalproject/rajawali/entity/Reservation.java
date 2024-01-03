e61f18230d47fbe0909208c158c8730729899a94
Dimas Wahyu Saputro <mas.dimas2910@gmail.com>
2024-01-04 02:44:38 +0700

    feat(entity): add base entity

---
+++ src/main/java/com/binarfinalproject/rajawali/entity/Reservation.java
@@ -0,0 +1,30 @@
+package com.binarfinalproject.rajawali.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "reservations")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "seat_id")
+    private Seat seat;
+
+    private LocalDateTime reservationDate;
+}
