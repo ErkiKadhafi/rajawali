2d1399f52acf3a7c0d5f56e1a9262baeb1761477
ErkiKadhafi <erki.qaedhafi@gmail.com>
2024-01-03 20:32:09 +0700

    init project

---
+++ src/main/java/com/binarfinalproject/rajawali/RajawaliApplication.java
@@ -0,0 +1,13 @@
+package com.binarfinalproject.rajawali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajawaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajawaliApplication.class, args);
+	}
+
+}
