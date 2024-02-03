365c22e70f038cfc505775d7944e9c944911482a
ErkiKadhafi <erki.qaedhafi@gmail.com>
2024-02-03 17:15:28 +0700

    allow cors

--- src/main/java/com/binarfinalproject/rajawali/RajawaliApplication.java
+++ src/main/java/com/binarfinalproject/rajawali/RajawaliApplication.java
@@ -2,6 +2,9 @@ package com.binarfinalproject.rajawali;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RajawaliApplication {
@@ -10,4 +13,13 @@ public class RajawaliApplication {
 		SpringApplication.run(RajawaliApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
